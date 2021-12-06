55952887f34afd905a689b6fdb8d8df2a8613773
yusufstawan <yusufsetiyawan45@gmail.com>
2021-12-06 21:13:51 +0700

    add file redirect

--- src/assets/parts/Footer.js
+++ src/assets/parts/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="flex text-center px-4 py-8 justify-center">
             <p className="text-sm">
-                Copyright 2021 • All Rights Reserved LuxSpace by BuildWith Angga
+                Copyright 2021
             </p>
         </footer>
     )
